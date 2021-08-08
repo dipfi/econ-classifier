8741e9e839f85c53fd84b2122890b4eaa6866283
damdurrer@gmail.com <41307083+dipfi@users.noreply.github.com>
2021-08-08 17:24:40 +0200

    CV for ortho vs hetero by journal, + feature importance

--- orthodox_heterodox/tfidf_classifier_ortho_hetero.py
+++ orthodox_heterodox/tfidf_classifier_ortho_hetero.py
@@ -67,6 +67,7 @@ from sklearn import feature_extraction, model_selection, naive_bayes, pipeline, 
 
 ## for balancing data
 from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 
 ## for explainer
 from lime import lime_text
@@ -102,8 +103,9 @@ text_field = "text"
 label_field = "y"
 cores = mp.cpu_count()  #mp.cpu_count()  #2
 save = False  # False #True
-plot = False
+plot = 1 #0 = none, 1 = some, 2 = all
 sentiment = False
+max_features = 1000
 ############################################
 
 
@@ -151,11 +153,11 @@ if __name__ == "__main__":
                           input_file_type = input_file_type,
                           sample_size = "all")
 
-
-fig, ax = plt.subplots()
-fig.suptitle("Label Distribution in Original Data", fontsize=12)
-dtf[label_field].reset_index().groupby(label_field).count().sort_values(by="index").plot(kind="barh", legend=False, ax=ax).grid(axis='x')
-plt.show()
+if plot == 1 or plot == 2:
+    fig, ax = plt.subplots()
+    fig.suptitle("Label Distribution in Original Data", fontsize=12)
+    dtf[label_field].reset_index().groupby(label_field).count().sort_values(by="index").plot(kind="barh", legend=False, ax=ax).grid(axis='x')
+    plt.show()
 
 
 
@@ -181,7 +183,7 @@ dtf.groupby(by="y")[["avg_word_length","avg_sentence_lenght", "sentence_count", 
 minimum_sample = min(dtf.groupby(by="y").size())
 
 
-if plot == True:
+if plot == 2:
     x, y = "avg_word_length", "y"
     fig, ax = plt.subplots(nrows=1, ncols=1)
     fig.suptitle(x, fontsize=12)
@@ -235,7 +237,7 @@ if sentiment == True:
     dtf["subjectivity"] = dtf["text_clean"].progress_apply(lambda x: TextBlob(x).sentiment.subjectivity) #0 = objective, 1=subjective
     dtf.groupby(by="y")[["polarity", "subjectivity"]].mean()
 
-    if plot == True:
+    if plot == 2:
         x, y = "polarity", "y"
         fig, ax = plt.subplots(nrows=1, ncols=1)
         fig.suptitle(x, fontsize=12)
@@ -277,7 +279,7 @@ dtf = dtf_length.copy()
 TRAIN TEST SPLIT
 '''
 logger.info("TRAIN TEST SPLIT")
-
+'''
 all_journals = dtf[["Source Title", label_field]].drop_duplicates().copy()
 heterodox_test = all_journals["Source Title"][all_journals[label_field] == "heterodox"].sample(frac=0.3).copy()
 samequality_test = all_journals["Source Title"][all_journals[label_field] == "samequality"].sample(frac=0.3).copy()
@@ -292,35 +294,41 @@ dtf_train = dtf[~dtf["Source Title"].isin(all_test)]
 
 dtf_train = dtf_train[[text_field_clean, label_field]]
 dtf_test = dtf_test[[text_field_clean, label_field]]
+'''
 
-#dtf_train, dtf_test = model_selection.train_test_split(dtf, test_size=0.3)
+
+dtf_train, dtf_test = model_selection.train_test_split(dtf, test_size=0.3)
 
 
 
 #balanbce dataset
 logger.info("BALANCE TRAINING SET")
-
+'''
 under_sampler = RandomUnderSampler(random_state=42)
 X_train, y_train = under_sampler.fit_resample(pd.DataFrame({"X": dtf_train[text_field_clean]}), pd.DataFrame({"y":dtf_train[label_field]}))
+'''
 
+over_sampler = RandomOverSampler(random_state=42)
+X_train, y_train = over_sampler.fit_resample(pd.DataFrame({"X": dtf_train[text_field_clean]}), pd.DataFrame({"y":dtf_train[label_field]}))
 X_train_vector = X_train
 y_train_vector = y_train
 
-fig, ax = plt.subplots()
-fig.suptitle("Label Distribution in Training Data", fontsize=12)
-y_train.value_counts().plot(kind="barh", legend=False, ax=ax).grid(axis='x')
-plt.show()
+if plot == 1 or plot == 2:
+    fig, ax = plt.subplots()
+    fig.suptitle("Label Distribution in Training Data", fontsize=12)
+    y_train.value_counts().plot(kind="barh", legend=False, ax=ax).grid(axis='x')
+    plt.show()
 
 
 #FEATURE ENGINEERING
 logger.info("FEATURE ENGINEERING")
 ## Count (classic BoW)
-#vectorizer = feature_extraction.text.CountVectorizer(max_features=1000, ngram_range=(1,2))
+#vectorizer = feature_extraction.text.CountVectorizer(max_features=max_features, ngram_range=(1,2))
 
 ## Tf-Idf (advanced variant of BoW)
 logger.info("TFIDF VECTORIZER")
 
-vectorizer = feature_extraction.text.TfidfVectorizer(max_features=1000, ngram_range=(1,2))
+vectorizer = feature_extraction.text.TfidfVectorizer(max_features=max_features, ngram_range=(1,2))
 
 corpus = X_train["X"]
 
@@ -328,7 +336,7 @@ vectorizer.fit(corpus)
 X_train = vectorizer.transform(corpus)
 dic_vocabulary = vectorizer.vocabulary_
 
-if plot == True:
+if plot == 2:
     sns.heatmap(X_train.todense()[:,np.random.randint(0,X_train.shape[1],100)]==0, vmin=0, vmax=1, cbar=False).set_title('Sparse Matrix Sample')
 
 
@@ -388,6 +396,44 @@ y_train = y_train.values.ravel()
 
 model["classifier"].fit(X_train, y_train)
 
+def get_salient_words(nb_clf, vect, class_ind, class_op):
+    """Return salient words for given class
+    Parameters
+    ----------
+    nb_clf : a Naive Bayes classifier (e.g. MultinomialNB, BernoulliNB)
+    vect : CountVectorizer
+    class_ind : int
+    Returns
+    -------
+    list
+        a sorted list of (word, log prob) sorted by log probability in descending order.
+    """
+
+    words = vect.get_feature_names()
+    zipped = list(zip(words, nb_clf.feature_log_prob_[class_ind], nb_clf.feature_log_prob_[class_op], np.log(np.exp(nb_clf.feature_log_prob_[class_ind])/np.exp(nb_clf.feature_log_prob_[class_op]))))
+    sorted_zip = sorted(zipped, key=lambda t: t[3], reverse=True)
+
+    return sorted_zip
+
+neg_salient_top_20 = get_salient_words(model["classifier"], vectorizer, 0, 1)[:20]
+pos_salient_top_20 = get_salient_words(model["classifier"], vectorizer, 1, 0)[:20]
+
+if plot == 1 or plot == 2:
+    fig, ax = plt.subplots()
+    fig.suptitle("Salient words for heterodox articles", fontsize=12)
+    plt.bar([i[0] for i in neg_salient_top_20], [i[3] for i in neg_salient_top_20])
+    plt.xticks(rotation='vertical')
+    fig.subplots_adjust(bottom=0.4)
+    plt.show()
+
+    fig, ax = plt.subplots()
+    fig.suptitle("Salient words for orthodox articles", fontsize=12)
+    plt.bar([i[0] for i in pos_salient_top_20], [i[3] for i in pos_salient_top_20])
+    plt.xticks(rotation='vertical')
+    fig.subplots_adjust(bottom=0.4)
+    plt.show()
+
+
 
 ## test
 logger.info("TEST CLASSIFIER")
@@ -421,50 +467,52 @@ print("Auc:", round(auc, 2))
 print("Detail:")
 print(metrics.classification_report(y_test, predicted))
 
-## Plot confusion matrix
-cm = metrics.confusion_matrix(y_test, predicted)
-fig, ax = plt.subplots()
-sns.heatmap(cm, annot=True, fmt='d', ax=ax, cmap=plt.cm.Blues,cbar=False)
-ax.set(xlabel="Pred", ylabel="True", xticklabels=classes,yticklabels=classes, title="Confusion matrix")
-plt.yticks(rotation=0)
-fig, ax = plt.subplots(nrows=1, ncols=2)
+if plot == 1 or plot == 2:
+    ## Plot confusion matrix
+    cm = metrics.confusion_matrix(y_test, predicted)
+    fig, ax = plt.subplots()
+    sns.heatmap(cm, annot=True, fmt='d', ax=ax, cmap=plt.cm.Blues,cbar=False)
+    ax.set(xlabel="Pred", ylabel="True", xticklabels=classes,yticklabels=classes, title="Confusion matrix")
+    plt.yticks(rotation=0)
+    fig, ax = plt.subplots(nrows=1, ncols=2)
 
-## Plot roc
-for i in range(len(classes)):
-    fpr, tpr, thresholds = metrics.roc_curve(y_test_array[:, i],predicted_prob[:, i])
-    ax[0].plot(fpr, tpr, lw=3,label='{0} (area={1:0.2f})'.format(classes[i],metrics.auc(fpr, tpr)))
-ax[0].plot([0, 1], [0, 1], color='navy', lw=3, linestyle='--')
-ax[0].set(xlim=[-0.05, 1.0], ylim=[0.0, 1.05],
-          xlabel='False Positive Rate',
-          ylabel="True Positive Rate (Recall)",
-          title="Receiver operating characteristic")
-ax[0].legend(loc="lower right")
-ax[0].grid(True)
+    ## Plot roc
+    for i in range(len(classes)):
+        fpr, tpr, thresholds = metrics.roc_curve(y_test_array[:, i],predicted_prob[:, i])
+        ax[0].plot(fpr, tpr, lw=3,label='{0} (area={1:0.2f})'.format(classes[i],metrics.auc(fpr, tpr)))
+    ax[0].plot([0, 1], [0, 1], color='navy', lw=3, linestyle='--')
+    ax[0].set(xlim=[-0.05, 1.0], ylim=[0.0, 1.05],
+              xlabel='False Positive Rate',
+              ylabel="True Positive Rate (Recall)",
+              title="Receiver operating characteristic")
+    ax[0].legend(loc="lower right")
+    ax[0].grid(True)
 
-## Plot precision-recall curve
-for i in range(len(classes)):
-    precision, recall, thresholds = metrics.precision_recall_curve(y_test_array[:, i], predicted_prob[:, i])
-    ax[1].plot(recall, precision, lw=3,label='{0} (area={1:0.2f})'.format(classes[i],metrics.auc(recall, precision)))
-ax[1].set(xlim=[0.0, 1.05], ylim=[0.0, 1.05], xlabel='Recall',ylabel="Precision", title="Precision-Recall curve")
-ax[1].legend(loc="best")
-ax[1].grid(True)
-plt.show()
+    ## Plot precision-recall curve
+    for i in range(len(classes)):
+        precision, recall, thresholds = metrics.precision_recall_curve(y_test_array[:, i], predicted_prob[:, i])
+        ax[1].plot(recall, precision, lw=3,label='{0} (area={1:0.2f})'.format(classes[i],metrics.auc(recall, precision)))
+    ax[1].set(xlim=[0.0, 1.05], ylim=[0.0, 1.05], xlabel='Recall',ylabel="Precision", title="Precision-Recall curve")
+    ax[1].legend(loc="best")
+    ax[1].grid(True)
+    plt.show()
 
 ## select observation
 logger.info("INTERPRETATION WITH LIME")
-for i in random.sample(range(1, len(y_train)), 10):
+for i in random.sample(range(1, len(dtf_test)), 10):
     txt_instance = dtf_test["text_clean"].iloc[i]
 
-    ## check true value and predicted value
-    print("True:", y_test[i], "--> Pred:", predicted[i], "| Prob:", round(np.max(predicted_prob[i]),2))
+    if plot == 1 or plot == 2:
+        ## check true value and predicted value
+        print("True:", y_test[i], "--> Pred:", predicted[i], "| Prob:", round(np.max(predicted_prob[i]),2))
 
-    import IPython
-    ## show explanation
-    explainer = lime_text.LimeTextExplainer(class_names=np.unique(y_train))
-    explained = explainer.explain_instance(txt_instance, model.predict_proba, num_features=9, top_labels=1)
-    explained.show_in_notebook(text=txt_instance, predict_proba=False)
-    name_of_html = "lime_" + os.path.basename(__file__) + "_observation_" + str(i) + ".html"
-    explained.save_to_file(data_path + "/lime_explanations/" + name_of_html)
+        import IPython
+        ## show explanation
+        explainer = lime_text.LimeTextExplainer(class_names=np.unique(y_train))
+        explained = explainer.explain_instance(txt_instance, model.predict_proba, num_features=9, top_labels=1)
+        explained.show_in_notebook(text=txt_instance, predict_proba=False)
+        name_of_html = "lime_" + os.path.basename(__file__) + "_observation_" + str(i) + ".html"
+        explained.save_to_file(data_path + "/lime_explanations/" + name_of_html)
 
 
 '''
@@ -472,7 +520,7 @@ WORD FREQUENCY
 '''
 print("WORD FREQUENCY")
 
-if plot == True:
+if plot == 2:
     top = 10
     y = "samequality"
     corpus = dtf[dtf["y"] == y]["text_clean"]
